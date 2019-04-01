8843bee893bd2ca7871a6a928324c561baa01ce2
Yongseok Jin <ysjin@davinci.snu.ac.kr>
2019-04-01 17:12:07 +0900

    added some comment for pc api

--- pc/pc_api.h
+++ pc/pc_api.h
@@ -5,6 +5,13 @@
 
 #define PC_SYSCALL 378
 
+/**
+ * user-level PC collection API.
+ * this only works on current desktop, which is patched to have system call number 378
+ * for PC collection.
+ * this stores PC information and the sum of PCs on separate file(/tmp/pc_syscall.log),
+ * and returns the sum of pc.
+ */
 #define get_pc() syscall(PC_SYSCALL)
 
 #endif
